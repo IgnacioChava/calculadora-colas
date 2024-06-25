c633be90cbb63140f1efbcdc92a57a965bb2293b
Jose Brenes <ignaciochavarria16@gmail.com>
2024-06-25 12:35:25 -0600

    creacion de calculadora

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Mm1 from './Pages/MM1/Mm1';
+import Mmm from './Pages/MMM/mmm';
+import MM1fin from './Pages/MM1Fin/MM1fin'
+import MD1 from './Pages/MD1/MD1'
+
+function App() {
+  
+
+
+  return (
+    <>
+      <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<Mm1></Mm1>} />
+                       <Route path="/MMM" element={<Mmm />} />
+                      
+                       <Route path="/MM1Fin" element={<MM1fin />} />
+                       <Route path="/MD1" element={<MD1 />} />
+                  <Route/>
+              </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/MD1/MD1.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import { InputNumber, Button, Form, Card, Typography } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Title, Text } = Typography;
+
+const MD1 = () => {
+    const [mu, setMu] = useState<number | null>(null);
+    const [lambda, setLambda] = useState<number | null>(null);
+
+    const [results, setResults] = useState<{
+        avgNumberInSystemL: number;
+        avgTimeInSystemW: number;
+        avgNumberInQueueLq: number;
+        avgTimeInQueueWq: number;
+        probabilityNoOneUsingSystemPo: number;
+    } | null>(null);
+
+    const calculateResults = () => {
+        if (mu && lambda) {
+
+            const avgNumberInQueueLq = ((lambda * lambda) / ((2 * mu) * (mu - lambda)));
+            const avgTimeInQueueWq = (lambda / ((2 * mu) * (mu - lambda)));
+            const avgNumberInSystemL = (avgNumberInQueueLq + (lambda / mu));
+            const avgTimeInSystemW = (avgTimeInQueueWq + (1 / mu));
+
+            const probabilityNoOneUsingSystemPo = 1 - (lambda / mu);
+
+            setResults({
+
+                avgNumberInSystemL,
+                avgTimeInSystemW,
+                avgNumberInQueueLq,
+                avgTimeInQueueWq,
+                probabilityNoOneUsingSystemPo
+            });
+        }
+    };
+
+    return (
+        <>
+
+            <Card title="M/D/1 Calculadora" style={{ maxWidth: 600, margin: 'auto', marginTop: 50 }}>
+
+                <Form layout="vertical">
+                    <Form.Item label="(μ)">
+                        <InputNumber min={0} value={mu ?? 0} onChange={setMu} />
+                    </Form.Item>
+                    <Form.Item label="(λ)">
+                        <InputNumber min={0} value={lambda ?? 0} onChange={setLambda} />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" onClick={calculateResults} disabled={mu === null || lambda === null}>
+                            Calcular
+                        </Button>
+                    </Form.Item>
+                </Form>
+                {results && (
+                    <div>
+                        <Title level={4}>Results:</Title>
+                        
+                        <Text>(L): {results.avgNumberInSystemL.toFixed(4)}</Text><br />
+                        <Text>(W): {results.avgTimeInSystemW.toFixed(4)}</Text><br />
+                        <Text>(Lq): {results.avgNumberInQueueLq.toFixed(4)}</Text><br />
+                        <Text>(Wq): {results.avgTimeInQueueWq.toFixed(4)}</Text><br />
+                        
+                    </div>
+                )}
+            </Card>
+            <div>
+                <h2>Menu</h2>
+                <div>
+                    <Button type='dashed'><Link to="/MMM"> MMM </Link></Button>
+                    <Button type='dashed'><Link to="/MM1Fin"> MM1 finito </Link></Button>
+                    <Button type='dashed'><Link to="/"> MM1 </Link></Button>
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default MD1;
